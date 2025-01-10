3f878481ea54c4ea3d1de38242ec6a1ac4d5f4a2
Jenny <jenisaro07@gmail.com>
2025-01-10 21:37:08 +0000

    cartas

--- src/app.js
+++ src/app.js
@@ -1,27 +1,22 @@
 function cardGenerate() {
-  const simbolos = ["♥", "♠", "♣", "♦"];
-  const numeros = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
+  let simbolos = ['♥', '♠', '♣', '♦'];
+  let numeros = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
-  const simboloAleatorio = Math.floor(Math.random() * simbolos.length);
-  const numeroAleatorio = Math.floor(Math.random() * numeros.length);
+  let simboloAleatorio = Math.floor(Math.random() * simbolos.length);
+  let numeroAleatorio = Math.floor(Math.random() * numeros.length);
 
   document.querySelector("#arriba").innerHTML = simbolos[simboloAleatorio];
   document.querySelector("#abajo").innerHTML = simbolos[simboloAleatorio];
-  document.querySelector("#numero").innerHTML = simbolos[numeroAleatorio];
-  let isRed =
-    simbolos[simboloAleatorio] == "♦" || simbolos[simboloAleatorio] == "♥";
+  document.querySelector("#numero").innerHTML = numeros[numeroAleatorio];
+
+  if (simbolos[simboloAleatorio] === '♦' || simbolos[simboloAleatorio] === '♥'){
+  document.querySelector('#arriba').className = 'display-5 text-danger';
+  document.querySelector('#abajo').className = 'text-danger';
+  } else {
+    document.querySelector('#arriba').className = 'display-5';
+    document.querySelector('#abajo').className = '';
+  }
 }
+
+window.onload = function () { cardGenerate();};
+setInterval(cardGenerate, 1000);
