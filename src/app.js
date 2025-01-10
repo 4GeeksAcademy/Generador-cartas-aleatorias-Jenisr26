ea35f2997208c1d3b37cddab6795a7e90d1ddd9f
Jenny <jenisaro07@gmail.com>
2025-01-10 20:35:58 +0000

    chaoprettier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function cardGenerate() {
+  const simbolos = ["♥", "♠", "♣", "♦"];
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const simboloAleatorio = Math.floor(Math.random() * simbolos.length);
+  const numeroAleatorio = Math.floor(Math.random() * numeros.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.querySelector("#arriba").innerHTML = simbolos[simboloAleatorio];
+  document.querySelector("#abajo").innerHTML = simbolos[simboloAleatorio];
+  document.querySelector("#numero").innerHTML = simbolos[numeroAleatorio];
+  let isRed =
+    simbolos[simboloAleatorio] == "♦" || simbolos[simboloAleatorio] == "♥";
+}
